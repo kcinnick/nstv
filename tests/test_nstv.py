0afaf9777c1f06eaabca6e8c4a4d69c9ee6f76d5
kcinnick <nicktucker4@gmail.com>
2021-05-03 00:04:07 -0400

    update test_nstv.py

--- tests/test_nstv.py
+++ tests/test_nstv.py
@@ -2,23 +2,41 @@
 
 """Tests for `nstv` package."""
 
-import pytest
+"""Main module."""
+import os
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from nstv import nstv
+from nstv.models import Base, Episode, Show
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
+def test_search_channels():
+    json_response = nstv.search_channels(start_channel=2, end_channel=29)
+    expected_channel_list = [2, 3, 4, 5, 6, 7, 8, 9, 11, 15, 16,
+                             17, 20, 22, 23, 24, 25, 27, 28, 29]
+    actual_channel_list = []
+    for item in json_response:
+        actual_channel_list.append(item['channel']['channelNumber'])
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    assert expected_channel_list == actual_channel_list
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_parse_search_channels_response():
+    database_url = (
+        f'postgresql://postgres:{os.getenv("POSTGRES_PASSWORD")}'
+        f'@127.0.0.1:5432/postgres'
+    )
+    engine = create_engine(database_url, echo=True)
+
+    #  create tables in models.py if they don't already exist
+    Base.metadata.create_all(engine)
+
+    #  create db session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    json_response = nstv.search_channels(start_channel=45, end_channel=46)
+    parsed_response = nstv.parse_search_channel_response(
+        db_session=session, response=json_response)
