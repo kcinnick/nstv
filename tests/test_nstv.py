e59f56423a030fc0b9f73cbb712cd62e295fd8cf
Nick Tucker <nicktucker4@gmail.com>
2021-05-07 18:30:20 -0400

    Update test_nstv.py

--- tests/test_nstv.py
+++ tests/test_nstv.py
@@ -16,8 +16,7 @@ from nstv.models import Base, Episode, Show
 def test_search_channels():
     json_response = nstv.search_channels(
         start_channel=2, end_channel=29, start_date='2021-05-04', end_date='2021-05-05')
-    from pprint import pprint
-    pprint(json_response)
+
     expected_channel_list = [2, 3, 4, 5, 6, 7, 8, 9, 11, 15, 16,
                              17, 20, 22, 23, 24, 25, 27, 28, 29]
     actual_channel_list = []
@@ -46,7 +45,7 @@ def test_parse_search_channels_response():
         response=json_response
     )
 
-    assert len(shows) > 5
+    assert len(shows) > 4
     assert len(episodes) > 10
 
 
@@ -64,19 +63,19 @@ def test_login_nzbgeek():
     nzbgeek.login()
     return
 
-
-def test_search_nzbgeek():
+@pytest.mark.skipif(type(os.getenv("POSTGRES_PASSWORD")) != str, reason="not on local, can't hit database.")
+def test_search_nzbgeek_for_episode():
     db_session = nstv.get_db_session()
     episode = db_session.query(Episode).where(Episode.title == 'Twins for the Win').first()
     nzbgeek = NZBGeek()
     nzbgeek.login()
-    nzbgeek.search_nzbgeek(episode)
+    nzbgeek.search_nzbgeek_for_episode(episode)
     return
 
-
+@pytest.mark.skipif(type(os.getenv("POSTGRES_PASSWORD")) != str, reason="not on local, can't hit database.")
 def test_get_nzb():
     nzbgeek = NZBGeek()
     nzbgeek.login()
     db_session = nstv.get_db_session()
     show = db_session.query(Show).where(Show.title == 'Chopped').first()
-    nzbgeek.get_nzb(show, season_number=1, episode_number=4)
+    nzbgeek.get_nzb(show, season_number=49, episode_number=3)
