1e4627294eabd80591dfd78a29a47cdf5b087280
kcinnick <nicktucker4@gmail.com>
2021-05-10 00:00:49 -0400

    add tests for get_missing_nzb, get_or_create_* methods

--- tests/test_nstv.py
+++ tests/test_nstv.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
 
 """Tests for `nstv` package."""
-import pytest
-
 import os
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+import pytest
 
 from nstv import nstv
 from nstv.download import NZBGeek
-from nstv.models import Base, Episode, Show
+from nstv.models import Episode, Show
 
 
 def test_search_channels():
@@ -24,6 +21,16 @@ def test_search_channels():
         actual_channel_list.append(item['channel']['channelNumber'])
 
     assert expected_channel_list == actual_channel_list
+
+
+def test_nzbg_login():
+    nzbg = NZBGeek()
+    nzbg.login()
+    #  go to dashboard and assert expected info is found
+    r = nzbg.session.get(
+        'https://nzbgeek.info/dashboard.php?myaccount'
+    )
+    assert 'my account' in str(r.content)
 
 
 @pytest.mark.skipif(type(os.getenv("POSTGRES_PASSWORD")) != str, reason="not on local, can't hit database.")
@@ -57,11 +64,13 @@ def test_episode_query():
     for episode in show.episodes[:2]:
         assert episode.title in ['Kitchen Cantina', 'Spice It Up!']
 
+
 @pytest.mark.skipif(type(os.getenv("POSTGRES_PASSWORD")) != str, reason="not on local, can't hit database.")
 def test_login_nzbgeek():
     nzbgeek = NZBGeek()
     nzbgeek.login()
     return
+
 
 @pytest.mark.skipif(type(os.getenv("POSTGRES_PASSWORD")) != str, reason="not on local, can't hit database.")
 def test_search_nzbgeek_for_episode():
@@ -72,10 +81,65 @@ def test_search_nzbgeek_for_episode():
     nzbgeek.search_nzbgeek_for_episode(episode)
     return
 
+
 @pytest.mark.skipif(type(os.getenv("POSTGRES_PASSWORD")) != str, reason="not on local, can't hit database.")
 def test_get_nzb():
     nzbgeek = NZBGeek()
     nzbgeek.login()
     db_session = nstv.get_db_session()
     show = db_session.query(Show).where(Show.title == 'Chopped').first()
-    nzbgeek.get_nzb(show, season_number=49, episode_number=3)
+    nzbgeek.get_nzb(show, season_number=48, episode_number=3)
+
+
+def test_get_missing_nzb():
+    nzbgeek = NZBGeek()
+    nzbgeek.login()
+    show = Show(title='Chopped', gid='85019')
+    with pytest.raises(AttributeError):
+        nzbgeek.get_nzb(show, season_number=-99, episode_number=-99)
+
+
+@pytest.mark.skipif(type(os.getenv("POSTGRES_PASSWORD")) != str, reason="not on local, can't hit database.")
+def test_get_or_create_show():
+    nzbgeek = NZBGeek()
+    nzbgeek.login()
+    db_session = nstv.get_db_session()
+    fake_listing = {'showName': 'thisisforthetest'}
+
+    fake_show = nstv.get_or_create_show(fake_listing, db_session)
+    #  assert show is now in the db
+    fake_show_entry = db_session.query(Show).where(Show.title == fake_show.title).first()
+    assert fake_show_entry
+
+    #  clean up fake show db entry
+    db_session.delete(fake_show)
+    db_session.commit()
+
+
+@pytest.mark.skipif(type(os.getenv("POSTGRES_PASSWORD")) != str, reason="not on local, can't hit database.")
+def test_get_or_create_episode():
+    nzbgeek = NZBGeek()
+    nzbgeek.login()
+    db_session = nstv.get_db_session()
+    fake_episode = {
+        'episodeTitle': 'thisepisodeisforthetest',
+        'listDateTime': '9999-04-04'
+    }
+    show = nstv.get_or_create_show(
+        listing={'showName': 'Chopped'}, db_session=db_session)
+    fake_episode = nstv.get_or_create_episode(fake_episode, show, db_session)
+    #  assert show is now in the db
+    fake_episode_entry = db_session.query(Episode).where(Episode.title == fake_episode.title).first()
+    assert fake_episode_entry
+
+    #  clean up fake show db entry
+    db_session.delete(fake_episode)
+    db_session.commit()
+
+
+@pytest.mark.skipif(type(os.getenv("POSTGRES_PASSWORD")) != str, reason="not on local, can't hit database.")
+def test_get_gid():
+    nzbgeek = NZBGeek()
+    nzbgeek.login()
+    db_session = nstv.get_db_session()
+    nzbgeek.get_gid('Chopped')
