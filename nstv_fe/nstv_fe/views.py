0b01c9d9bd02d99f2e671b408ef18c51f22ab992
kcinnick <nicktucker4@gmail.com>
2021-05-15 00:24:20 -0400

    updated views.py and tox.ini

--- nstv_fe/nstv_fe/views.py
+++ nstv_fe/nstv_fe/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
 from .forms import DownloadForm
-from .models import Show
+from .models import Show, Episode
+from .tables import ShowTable
 
 
 def index(request):
-    index_context = {
-        "title": "Dashboard",
-        "download_form": DownloadForm()
-    }
     from nstv.download import NZBGeek
     nzb_geek = NZBGeek()
     nzb_geek.login()
@@ -26,9 +23,69 @@ def index(request):
 
             print(f"Downloading {show.title} S{season_number} E{episode_number}..")
             try:
-                nzb_geek.get_nzb(show, season_number=season_number, episode_number=episode_number)
+                nzb_geek.get_nzb(
+                    show, season_number=season_number, episode_number=episode_number)
             except AttributeError:
                 #  no download link found
                 print('No download link found.  Returning to index.')
 
+    index_context = {
+        "title": "Dashboard",
+        "download_form": DownloadForm()
+    }
+
     return render(request, '/home/nick/PycharmProjects/nstv/nstv_fe/templates/index.html', index_context)
+
+
+def shows_index(request):
+    show_table = ShowTable(
+        Show.objects.all().order_by('id')
+    )
+    show_table.paginate(page=request.GET.get("page", 1), per_page=10)
+
+    index_context = {
+        "title": "Show Index",
+        "shows": show_table
+    }
+
+    return render(request, '/home/nick/PycharmProjects/nstv/nstv_fe/templates/shows_index.html', index_context)
+
+
+def show(request, sid):
+    show_ = Show.objects.filter(id=sid).first()
+    episodes = Episode.objects.filter(show=show_)
+    index_context = {
+        "title": "Show",
+        "show": show_,
+        "episodes": episodes
+    }
+
+    return render(request, '/home/nick/PycharmProjects/nstv/nstv_fe/templates/show.html', index_context)
+
+
+def download_episode(request, sid, eid):
+    print(eid, sid)
+
+    from nstv.download import NZBGeek
+    nzb_geek = NZBGeek()
+    nzb_geek.login()
+    episode = Episode.objects.get(id=eid)
+    show_ = Show.objects.get(id=sid)
+    print('episode title: {} ~'.format(episode.title))
+    if episode.title:
+        nzb_geek.get_nzb(
+            show=show_, episode_title=episode.title
+        )
+    else:
+        print(
+            'Searching shows by season or episode number '
+            'isn\'t currently supported.\n')
+        raise NotImplementedError
+
+    index_context = {
+        "title": "Show",
+        "show": show,
+        "episodes": []
+    }
+
+    return render(request, '/home/nick/PycharmProjects/nstv/nstv_fe/templates/show.html', index_context)
