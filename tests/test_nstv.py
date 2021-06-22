86f9c66fe2706be0c3ef85a29f899e77e987f367
kcinnick <nicktucker4@gmail.com>
2021-06-22 18:45:31 -0400

    update .travis.yml

--- tests/test_nstv.py
+++ tests/test_nstv.py
@@ -166,7 +166,7 @@ def test_get_gid():
 
 def test_login_bounces_if_already_logged_in():
     print(
-        os.environ['NZBGEEK_USERNAME']
+        os.getenv('NZBGEEK_USERNAME')
     )
     nzbgeek = NZBGeek()
     nzbgeek.login()
