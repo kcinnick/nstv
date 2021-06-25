e19e1ac1e1c64b962f6035e83f41dd989b8b3a1a
kcinnick <nicktucker4@gmail.com>
2021-06-25 16:31:39 -0400

    put skip on test for model

--- tests/test_nstv_models.py
+++ tests/test_nstv_models.py
@@ -1,7 +1,14 @@
+import os
+
+import pytest
+
 from nstv import models
 from nstv.nstv import get_db_session
 
+SKIP_REASON = "not on local, can't hit database."
 
+
+@pytest.mark.skipif(type(os.getenv("POSTGRES_PASSWORD")) != str, reason=SKIP_REASON)
 def test_episode_model():
     db_session = get_db_session()
     episode = db_session.query(models.Episode).where(models.Episode.id == 1).first()
