15328f8aba70844d1fcd8d0063b6584344c4876c
kcinnick <nicktucker4@gmail.com>
2023-09-07 16:36:49 -0400

    - rename `plexController/main.py` to `add_shows_to_nstv.py`

---
+++ nstv/plexController/add_shows_to_nstv.py
@@ -0,0 +1,36 @@
+import os
+
+import django
+from plexapi.myplex import MyPlexAccount
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject.settings')
+django.setup()
+from nstv.models import Show, Episode
+
+account = MyPlexAccount('nicktucker4@gmail.com', os.getenv('PLEX_API_KEY'))
+plex = account.resource(os.getenv('PLEX_SERVER')).connect()  # returns a PlexServer instance
+
+
+def add_shows_to_nstv():
+    plex_tv_shows = plex.library.section('TV Shows')
+    for show in plex_tv_shows.search():
+        print(show.title)
+        show_object = Show.objects.all().filter(title=show.title)
+        if show_object:
+            print('show already exists')
+        else:
+            print('show does not exist')
+            show_object = Show(title=show.title)
+            show_object.save()
+            print('show added to database')
+
+    return
+
+
+def main():
+    add_shows_to_nstv()
+    return
+
+
+if __name__ == '__main__':
+    main()
