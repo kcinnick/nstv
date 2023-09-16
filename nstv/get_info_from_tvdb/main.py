d81c0acb985ba7067dec09137017dab8080d78cd
kcinnick <nicktucker4@gmail.com>
2023-09-15 22:25:45 -0400

    - add logic for getting tv show info for episodes that are/aren't on disk from tvdb

---
+++ nstv/get_info_from_tvdb/main.py
@@ -0,0 +1,57 @@
+import os
+import django
+from pprint import pprint
+import tvdb_v4_official
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject.settings')
+django.setup()
+
+from nstv.models import Show, Episode
+
+def find_tvdb_record_for_series(tvdb_api, series_name):
+    items = tvdb_api.search(query=series_name, type='series', language='eng')
+    print(items)
+    for i in items:
+        translations = i.get('translations')
+        englishTranslation = translations['eng']
+        if englishTranslation == series_name:
+            return i
+
+
+def main():
+    tvdb = tvdb_v4_official.TVDB(os.getenv('TVDB_API_KEY'))
+    shows = Show.objects.get(title='The Secret Life of the Zoo')
+    shows = [shows]
+    for show in shows:
+        nstv_episodes = Episode.objects.filter(show=show)
+        print(show.episodes)
+        print(show)
+        tvdb_record = find_tvdb_record_for_series(tvdb, show.title)
+        tvdb_series = tvdb.get_series(tvdb_record['id'].split('-')[1])
+        tvdb_series_episodes = tvdb.get_series_episodes(tvdb_series['id'], lang='eng')
+        tvdb_episode_listings = tvdb_series_episodes['episodes']
+        for tvdb_episode_listing in tvdb_episode_listings:
+            match = False
+            print('---')
+            print(tvdb_episode_listing)
+            for nstv_episode in nstv_episodes:
+                print('Checking if {} == {}'.format(nstv_episode.title, tvdb_episode_listing['name']))
+                if nstv_episode.title == tvdb_episode_listing['name']:
+                    print('Matched {} with {}'.format(nstv_episode.title, tvdb_episode_listing['name']))
+                    match = True
+                    break
+            if not match:
+                print('No match found for {}'.format(tvdb_episode_listing['name']))
+                # print('Creating episode for {}'.format(tvdb_episode_listing['name']))
+                # Episode.objects.create(
+                #     show=show,
+                #     air_date=tvdb_episode_listing['aired'],
+                #     title=tvdb_episode_listing['name'],
+                #     season_number=tvdb_episode_listing['seasonNumber'],
+                #     number=tvdb_episode_listing['number'],
+                # )
+    return
+
+
+if __name__ == '__main__':
+    main()
