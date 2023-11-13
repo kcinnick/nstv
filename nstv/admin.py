bd1c3f2ca35be62f78d666a163bc10c4118b5e9b
kcinnick <nicktucker4@gmail.com>
2023-11-13 18:49:52 -0500

    - register models in `admin.py`

--- nstv/admin.py
+++ nstv/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Show, Episode, Movie, CastMember
 
 # Register your models here.
+admin.site.register(Show)
+admin.site.register(Episode)
+admin.site.register(Movie)
+admin.site.register(CastMember)
