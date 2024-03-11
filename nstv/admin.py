15f3cd619bdfcf8d0fada8916b192f9bcbf7c0a1
kcinnick <nicktucker4@gmail.com>
2024-03-11 14:49:02 -0400

    - add `NZBDownloads` table

--- nstv/admin.py
+++ nstv/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Show, Episode, Movie, CastMember
+from .models import Show, Episode, Movie, CastMember, NZBDownload
 
 # Register your models here.
 admin.site.register(Show)
 admin.site.register(Episode)
 admin.site.register(Movie)
 admin.site.register(CastMember)
+admin.site.register(NZBDownload)
